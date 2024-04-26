3d02b6f21d56f53903adb6532e9cd7711adca331
Roman <rv920490@gmail.com>
2024-04-26 07:57:52 +0500

    v26/04

--- user_data/src/model/Requests.java
+++ user_data/src/model/Requests.java
@@ -1,21 +1,23 @@
 package model;
 
 public enum Requests {
-    request1("имя",10),
-    request2("фамилию",15),
-    request3("отчество",10),
-    request4("день рождения",2),
-    request5("месяц рождения",2),
-    request6("год рождения",4),
-    request7("телефон с кодом страны 8",11),
-    request8("ваш пол f или m",1);
+    request1("имя",10, 0),
+    request2("фамилию",15, 0),
+    request3("отчество",10, 0),
+    request4("день рождения",2, 1),
+    request5("месяц рождения",2, 1),
+    request6("год рождения",4, 1),
+    request7("телефон с кодом страны 8",11, 1),
+    request8("ваш пол f или m",1, 1);
 
     public final String text;
     public final int number;
+    public final int check;
 
 
-    Requests(String text, int number) {
+    Requests(String text, int number, int check) {
         this.text = text;
         this.number = number;
+        this.check = check;
     }
 }
--- user_data/src/presenter/CreateUser.java
+++ user_data/src/presenter/CreateUser.java
@@ -10,29 +10,36 @@ import java.util.List;
 import java.util.Scanner;
 
 public class CreateUser {
-    public String request(String text) {
+    public String request(String text, Requests e) {
         View requestView = new View();
-        boolean flag;
-        for(Requests e : Requests.values()) {
-            flag = true;
-            while (flag) {
-                requestView.viewRequest(text+" "+e.text);
-                if()
+        String answer= null;
+        boolean flag = true;
+        while (flag) {
+            requestView.viewRequest(text + " " + e.text);
+            try (Scanner isScanner = new Scanner(System.in)) {
+                answer = isScanner.nextLine();
             }
-
+            switch (e.check) {
+                case 1:
+                    if (e.number == answer.length()) {
+                        flag = false;
+                    } else
+                case 0:
+                    if (e.number >= answer.length()) {
+                        flag = false;
+                    } else
+            }
         }
-
-        try (Scanner isScanner = new Scanner(System.in)) {
-            return isScanner.nextLine();
-        }
+        return answer;
     }
+}
+}
 
-    public ArrayList<String> dataList(String text) {
-        ArrayList <String> list = new ArrayList<>();
+public ArrayList<String> dataList(String text) {
+    ArrayList<String> list = new ArrayList<>();
 
 
-
-    }
+}
 
 
 }
